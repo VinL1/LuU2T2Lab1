58fdd359b31f182259127f2d5a6c2ac5f48eb22e
Vincent Lu <vincentl105@nycstudents.net>
2022-10-03 17:36:27 -0400

    using "setters"

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -5,33 +5,12 @@ public class RectangleRunner {
         Rectangle plot2 = new Rectangle (125);
         Rectangle plot3 = new Rectangle ();
 
-        System.out.println ("Plot 1 Length: " + plot1.getLength ());
-        System.out.println ("Plot 1 Width: " + plot1.getWidth());
-        System.out.println ("Plot 1 Area: " + plot1.calculateArea());
-        System.out.println ();
-        System.out.println ("Plot 2 Length: " + plot2.getLength ());
-        System.out.println ("Plot 2 Width: " + plot2.getWidth());
-        System.out.println ("Plot 2 Area: " + plot2.calculateArea());
-        System.out.println ();
-        System.out.println ("Plot 3 Length: " + plot3.getLength ());
-        System.out.println ("Plot 3 Width: " + plot3.getWidth());
-        System.out.println ("Plot 3 Area: " + plot3.calculateArea());
-
         plot1.setWidth (75);
         plot2.setLength (75);
         plot2.setWidth (75);
         plot3.setWidth (75);
 
-        System.out.println ("Plot 1 Length: " + plot1.getLength ());
-        System.out.println ("Plot 1 Width: " + plot1.getWidth());
-        System.out.println ("Plot 1 Area: " + plot1.calculateArea());
-        System.out.println ();
-        System.out.println ("Plot 2 Length: " + plot2.getLength ());
-        System.out.println ("Plot 2 Width: " + plot2.getWidth());
-        System.out.println ("Plot 2 Area: " + plot2.calculateArea());
-        System.out.println ();
-        System.out.println ("Plot 3 Length: " + plot3.getLength ());
-        System.out.println ("Plot 3 Width: " + plot3.getWidth());
-        System.out.println ("Plot 3 Area: " + plot3.calculateArea());
+        int sum = plot1.calculateArea () + plot2.calculateArea () + plot3.calculateArea ();
+        System.out.println ("These three plots requires " + sum + " square feet of seed");
     }
 }
