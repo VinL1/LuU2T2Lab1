be0a593f74ad6a58be59e60a415a3e9ebd195eb8
Vincent Lu <vincentl105@nycstudents.net>
2022-10-03 17:29:50 -0400

    using "setters"

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -16,5 +16,22 @@ public class RectangleRunner {
         System.out.println ("Plot 3 Length: " + plot3.getLength ());
         System.out.println ("Plot 3 Width: " + plot3.getWidth());
         System.out.println ("Plot 3 Area: " + plot3.calculateArea());
+
+        plot1.setWidth (75);
+        plot2.setLength (75);
+        plot2.setWidth (75);
+        plot3.setWidth (75);
+
+        System.out.println ("Plot 1 Length: " + plot1.getLength ());
+        System.out.println ("Plot 1 Width: " + plot1.getWidth());
+        System.out.println ("Plot 1 Area: " + plot1.calculateArea());
+        System.out.println ();
+        System.out.println ("Plot 2 Length: " + plot2.getLength ());
+        System.out.println ("Plot 2 Width: " + plot2.getWidth());
+        System.out.println ("Plot 2 Area: " + plot2.calculateArea());
+        System.out.println ();
+        System.out.println ("Plot 3 Length: " + plot3.getLength ());
+        System.out.println ("Plot 3 Width: " + plot3.getWidth());
+        System.out.println ("Plot 3 Area: " + plot3.calculateArea());
     }
 }
